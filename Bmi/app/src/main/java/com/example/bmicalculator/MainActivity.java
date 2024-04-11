a03233eb049dd5c94f124b63b96ac1a3b540d39b
sanchi285 <bismoy.sanchi@gmail.com>
2024-04-12 00:21:44 +0600

    All Id added for xml content and button click added

--- Bmi/app/src/main/java/com/example/bmicalculator/MainActivity.java
+++ Bmi/app/src/main/java/com/example/bmicalculator/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +19,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        String alertTest = "This is my Variable Text";
+        //Toast.makeText(this,alertTest,Toast.LENGTH_LONG)
+        //        .show();
+        TextView resultText =  findViewById(R.id.text_view_result);
+
+        RadioButton malebutton = findViewById(R.id.radio_button_male);
+        RadioButton femalebutton = findViewById(R.id.radio_button_female);
+
+        EditText ageText = findViewById(R.id.text_age);
+        EditText feetText = findViewById(R.id.text_feet);
+        EditText inchesText = findViewById(R.id.text_inches);
+        EditText  weightText = findViewById(R.id.text_weight);
+
+        Button calculateButton = findViewById(R.id.button_calculate);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You clicked",Toast.LENGTH_LONG)
+                        .show();
+            }
         });
+
     }
 }
